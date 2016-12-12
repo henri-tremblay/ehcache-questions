a3fe6771caf5c7d33968f112f9d4d753b53cf6d9
Henri Tremblay <henri.tremblay@gmail.com>
2016-12-12 11:05:11 -0500

    Initial commit

---
+++ src/main/java/pro/tremblay/ehcachequestions/App.java
@@ -0,0 +1,13 @@
+package pro.tremblay.ehcachequestions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
